a696779c97db0eb243b4b1ff2b94b0062634bd1d
belhazzh <belhassenzzh5@gmail.com>
2023-06-05 02:51:10 +0100

    db missconf

--- gestion_formation/settings.py
+++ gestion_formation/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '1872000zzH5*',
-        'HOST': '34.159.71.70',  # If running on the local machine, use 'localhost' or '127.0.0.1'
+        'HOST': '34.122.229.238',  # If running on the local machine, use 'localhost' or '127.0.0.1'
         'PORT': '5432',  # Default PostgreSQL port is '5432'
     }
 }
